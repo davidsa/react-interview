24523ff40a527968b83729ac8414096aca424fd7
David Sttivend <dsangel@gmail.com>
2026-02-09 11:21:59 -0500

    Fix bgColor

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import UserForm from "./UserForm";
 
 function App() {
   return (
-    <div className="h-full w-full bg-gray-14 flex flex-col items-center py-10 gap-10">
+    <div className="h-full w-full flex flex-col items-center py-10 gap-10">
       <div className="flex flex-col items-center w-2xl gap-5">
         <h1 className="text-2xl text-white">User Form</h1>
         <UserForm showTOS={true} isCreating={true} />
@@ -20,7 +20,7 @@ function App() {
       </div>
       <div className="flex flex-col items-center w-2xl gap-5">
         <h1 className="text-2xl text-white">Banner User Form</h1>
-        <UserForm fields={["email"]} isBanner={true} />
+        <UserForm isBanner={true} />
       </div>
     </div>
   );
